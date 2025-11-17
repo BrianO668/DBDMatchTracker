0f475ec8998c6eaeaf19c0d0831f00d096a4a140
Brian <70177580+BrianO668@users.noreply.github.com>
2025-11-17 14:29:40 -0500

    Update Main.java
    JavaDoc support

--- Main.java
+++ Main.java
@@ -1,8 +1,17 @@
 import javax.swing.*;
 
+/**
+ * Main Class: Includes main method used to start the application
+ */
 public class Main {
+    /**
+     * main method: Starts a new Swing thread
+     */
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() { //Thread for running our MainWindow GUI
+            /**
+             * run method: Is part of the new Swing thread. Instantiates a MainWindow. Essentially paints our window (designed in another class) to the screen.
+             */
             public void run() {
                 MainWindow mainWindow = new MainWindow();
             }
